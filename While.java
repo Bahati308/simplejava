beeae2bfee9b4a65a2776501219b41742f45724f
@Bahati308 <bahatibrianp@gmail.com>
2023-01-20 18:40:15 +0300

    Added While

---
+++ While.java
@@ -0,0 +1,14 @@
+/*
+ * Demonstration of the while loop
+ * this is java's most fundamental loop statement.
+ */
+public class While {
+    public static void main(String[] args) {
+        int n=10;
+        while (n>0) {
+            System.out.println("tick "+ n);
+            n--;
+        }
+        
+    }
+}
