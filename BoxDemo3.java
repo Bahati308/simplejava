8664dc4abf935fb7b02b17aa0786e78f47f6b72d
@Bahati308 <bahatibrianp@gmail.com>
2023-01-25 14:55:57 +0300

    Added BoxDemo3.java

---
+++ BoxDemo3.java
@@ -0,0 +1,39 @@
+// this program includes a method inside the Box class.
+class Box {
+    double width;
+    double height;
+    double depth;
+
+    // display volume of the box
+    void volume (){
+        System.out.println("Volume is ");
+        System.out.println(width*depth*height);
+    }
+}
+
+public class BoxDemo3 {
+    public static void main(String[] args) {
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+
+    // assigning values to mybox1 instance variables
+
+    mybox1.depth=10;
+    mybox1.height=20;
+    mybox1.width=30;
+
+    // assigning values to mybox2 instance variables
+
+    mybox2.depth=3;
+    mybox2.height=6;
+    mybox2.width=9;
+
+        //display volume of the first box
+
+        mybox1.volume();
+
+        //display volume of the second box
+
+        mybox2.volume();
+    }
+}
