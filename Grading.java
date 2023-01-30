b9cf67ef16512ba992fba318808a26a3c4840658
@Bahati308 <bahatibrianp@gmail.com>
2023-01-30 17:58:38 +0300

    added Grading.java

---
+++ Grading.java
@@ -0,0 +1,44 @@
+/*this program grades simple class results
+ * developed by Bahati308
+ * Mon Jan 30, 2023
+ */
+
+ //this imports the java.util which will be usefull later on
+import java.util.*;
+
+public class Grading {
+    public static void main(String[] args) {
+        // the above java.util helps us use this 
+        // and enables us get input from the keyboard
+        Scanner scan = new Scanner(System.in);
+
+        //simply declaring varibles
+        float eng, math, sci, prog;
+
+        System.out.print("Programming: " );
+        prog = scan.nextFloat();
+        System.out.print("Science: ");
+        sci = scan.nextFloat();
+        System.out.print("Math: ");
+        math =scan.nextFloat();
+        System.out.print("English: ");
+        eng =scan.nextFloat();
+
+    //we are finding out the averages
+        float average = ((eng+math+sci+prog)/4);
+        System.out.println("Total Averages = "+average);
+
+        if(average == 100){
+            System.out.println("Excellent Work");
+        }
+        else if(average >=85 ){
+            System.out.println("Impressive");
+        }
+        else if(average >= 65){
+            System.out.println("You did great");
+        }
+        else {
+            System.out.println("Do not settle for less");
+        }
+    }
+}
